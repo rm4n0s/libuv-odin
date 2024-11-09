7b2de6bcfac733a2fceda3a523ece6205d08a79f
PucklaJ <kassandra@pucher.space>
2024-11-09 14:16:16 +0900

    Add c stdlib and pthread stub headers

---
+++ prepocess/src/pthread.h
@@ -0,0 +1,6 @@
+typedef int pthread_once_t;
+typedef int pthread_t;
+typedef char pthread_mutex_t[48];
+typedef int pthread_cond_t;
+typedef int pthread_key_t;
+
---
+++ prepocess/src/sys/types.h
@@ -0,0 +1,5 @@
+typedef int pid_t;
+typedef int gid_t;
+typedef int uid_t;
+
+typedef int sem_t;
